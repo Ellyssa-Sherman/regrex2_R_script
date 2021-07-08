d04c17c8527d453f9fb454c66d879f8e347e670e
Ellyssa Sherman <ellyssa.sherman@gmail.com>
2021-07-08 21:59:04 +0000

    Modify script for submission

--- Linear Model_R.r
+++ Linear Model_R.r
@@ -1,19 +1,28 @@
-dataset <- read.csv("regrex1.csv",
-                    header = TRUE)
+args <- commandArgs(trailingOnly = TRUE)
+
+# Call in data for plotting with argument line (regression same as Python exercise) 
+dataset <- read.csv(args[1])
 dataset
 
 y1 <- dataset[,1]
 x1 <- dataset[,2]
 
-# Plot data points
+# Plot data points, opening png device.
+png('Data Scatter Plot_R.png')
 plot(y1,
      x1,
-     ylab="Y",
-     xlab="X")
+     ylab='Y',
+     xlab='X',
+     main = 'Regrex Data')
+dev.off() # closing png device
 
 # Create linear regression model, plot points, and add regression
+# Opening png device
+png('Data Linear Regression Plot_R.png')
 lmRegrex = lm(x~y, data = dataset)
 plot(dataset,
     pch = 16,
-    col = "black")
+    col = 'black',
+    main = 'Regrex Data')
 abline(lmRegrex)
+dev.off()  # closing png device
